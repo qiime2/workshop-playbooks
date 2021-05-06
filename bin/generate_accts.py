0dd8676a9008a50727625be0c110c692ae56293e
Liz Gehret <54517601+lizgehret@users.noreply.github.com>
2021-05-06 09:21:37 -0700

    Update generate_accts.py

--- bin/generate_accts.py
+++ bin/generate_accts.py
@@ -1,4 +1,4 @@
-# !/usr/bin/env python
+#!/usr/bin/env python
 # ----------------------------------------------------------------------------
 # Copyright (c) 2016-2021, QIIME 2 development team.
 #
